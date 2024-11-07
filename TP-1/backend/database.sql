8138d55bb898d77f035a0f0a7ab5f6d6ed5a5172
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2024-11-07 22:42:34 +0100

    lab1

--- TP-1/backend/database.sql
+++ TP-1/backend/database.sql
@@ -1,6 +1,16 @@
 CREATE TABLE book (title character varying(100) NOT NULL);
 INSERT INTO book (title)
-VALUES ('DevOps'),
-    ('Questionmark'),
-    ('Cloud'),
-    ('CI/CD');
+VALUES ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet'),
+    ('Sébastien Meynet');
